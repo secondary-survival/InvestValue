758da00c4aa7d0fec889cc5a916b87110380a8d5
secondary-survival <133509046+secondary-survival@users.noreply.github.com>
2023-05-13 18:44:36 +0530

    Add files via upload

--- Invest Value/NeoFinDeskAutomation/src/main/java/base/AutomationBase.java
+++ Invest Value/NeoFinDeskAutomation/src/main/java/base/AutomationBase.java
@@ -2,6 +2,8 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 
 import static data.DataProvider.dataProvider;
 import static utils.Browser.Capability.setOptions;
@@ -9,13 +11,18 @@ import static utils.Browser.Capability.setOptions;
 /**
  * This is an automation base class for all UI automation scripts.
  */
-public class AutomationBase {
+class AutomationBase {
     static WebDriver driver;
 
-    public static void main(String[] args) {
+    @BeforeSuite
+    public static void suiteSetup() {
         System.setProperty("webdriver.chrome.driver", dataProvider("chrome.driver.path"));
         driver = new ChromeDriver(setOptions());
         driver.get(dataProvider("url"));
+    }
+
+    @AfterSuite
+    public static void suiteTearDown(){
         driver.quit();
     }
 }
